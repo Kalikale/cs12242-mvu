7432c2562a2b1bdfd440183f5ceb316417ca9ee6
Louie Torres <dev@lowkeytowers.com>
2025-05-31 10:10:22 +0800

    Fix image loading

--- src/canvas.ts
+++ src/canvas.ts
@@ -220,15 +220,12 @@ export const canvasView =
                             cache,
                             HashMap.get(src),
                             Option.getOrElse(() => {
-                              // FIXME: Must wait for load event
                               const ret = new Image()
                               ret.src = src
-                              return ret
-                            }),
-                            (img) =>
-                              Effect.void.pipe(
-                                Effect.tap(() => ctx.drawImage(img, x, y)),
-                              ),
+                              ret.onload = () => {
+                                ctx.drawImage(ret, x, y)
+                              }
+                            })
                           ),
                         ),
                       ),
